c21f49dad90216ff2264b36c8b150863a4862f08
Thanirin Trironnarith <36672273+imrinzzzz@users.noreply.github.com>
2019-11-26 22:40:50 +0700

    Add files via upload

---
+++ firstProg.py
@@ -0,0 +1,4 @@
+print("This is my first Python program")
+num1= 2; num2=3
+sum = num1 + num2
+print(f"The result of {num1} + {num2} = {sum}")
---
+++ lab01.py
@@ -0,0 +1,5 @@
+ages = [19, 20, 18]
+for x in ages:
+    sum += x
+sum /=len(ages)
+print(sum)
